0a2d227ee24c0b317ca7e25f71a71b202cef50d4
Recyclingbottle <hoholeo382@gmail.com>
2024-08-05 20:51:26 +0900

    Initial commit 🎉

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './image.png';
+import kakaologin from './kakao_login_medium_wide.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>제주 꼬망이들</h1>
+        <button className="kakao-login">
+          <img src={kakaologin} alt="카카오 로그인" className="kakao-login-image" />
+        </button>
       </header>
     </div>
   );
