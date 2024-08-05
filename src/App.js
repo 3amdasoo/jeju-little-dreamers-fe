33f48557def7691327c78cb699475ca1c6d33eb6
Recyclingbottle <hoholeo382@gmail.com>
2024-08-05 23:15:51 +0900

    test 페이지( 컴포넌트 개발용)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Main from "./pages/Main";
+import Test from "./pages/test";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
           <Route path={"/main"} element={<Main />}></Route>
+          <Route path={"/test"} element={<Test/>}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/pages/test.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.png";
+import kakaologin from "../assets/kakao_login.png";
+
+export default function test() {
+  return (
+    <div>
+    </div>
+  );
+}
