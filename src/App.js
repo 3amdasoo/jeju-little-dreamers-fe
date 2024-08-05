97377f1b6570adbeae6627e16947f66b4080d087
lucy726j <lucy726jeon@gmail.com>
2024-08-05 21:41:18 +0900

    헤더 레이아웃

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Main from "./pages/Main";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
+          <Route path={"/main"} element={<Main />}></Route>
         </Routes>
       </Router>
     </div>
